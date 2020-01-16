4cb43beef26fb037ec35918b6d329908150b00df
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2020-01-16 00:53:55 -0300

    base of application with aside bar on development

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import './global.css';
 import './App.css';
+import Header from './Header';
 
 function App() {
+  const [counter, setCounter] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong></strong>
+        <form>
+          <div class="input-block">
+            <label htmlFor="github_username">Usuário do github</label>
+            <input name="github_username" id="github_username" required  />
+          </div>
+          <div class="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required  />
+          </div>
+          
+          <div class="input-group">
+            <label htmlFor="username_github">Usuário do github</label>
+            <input name="username_github" id="username_github" required  />
+          </div>
+        </form>
+        
+      </aside>
+      <main></main>
     </div>
   );
 }
