db2b2bc9fcb664035b2a0174a6ff48bddd2134b2
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2020-01-17 01:05:11 -0300

    styles on boxes get coordinates and setting navigator is with error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import './App.css';
 import './global.css';
@@ -8,29 +8,50 @@ import './sidebar.css';
 
 function App() {
 
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+  useEffect(() =>{
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const {latitude, longitude} = position.coords;
+
+        setLatitude(latitude)
+        setLongitude(longitude)
+      },
+      (error) => {
+        console.log(error)
+      },
+      {
+        timeout: 30000
+      }
+    );
+  },
+  [])
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
         <form>
-          <div class="input-block">
+          <div className="input-block">
             <label htmlFor="github_username">Usuário do github</label>
             <input name="github_username" id="github_username" required  />
           </div>
-          <div class="input-block">
+          <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
             <input name="techs" id="techs" required  />
           </div>
           
-          <div class="input-group">
-            <div class="input-block">
+          <div className="input-group">
+            <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required  />
+              <input type="number" name="latitude" id="latitude" required value={latitude} />
             </div>
 
-            <div class="input-block">
+            <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required  />
+              <input type="number" name="longitude" id="longitude" required value={longitude} />
             </div>
           </div>
           <button type="submit">Salvar</button>
